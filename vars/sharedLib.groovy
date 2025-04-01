050cc4fa3ce57b04b28b7637e9a74bbb0d950293
Bharath Kumar N R <Bharath.Kumar.N.R@ibm.com>
2025-04-01 10:59:45 +0530

    updated vars/sharedLib.groovy

--- vars/sharedLib.groovy
+++ vars/sharedLib.groovy
@@ -3,12 +3,3 @@ def build()
     sh 'mvn clean package -DskipTests=true'
     stash name: 'buildArtifacts', includes: '**/target/*.jar'
 }
-def test()
-{
-    sh 'mvn test'
-}
-
-def deploy()
-{
-    sh 'java -jar $WORKSPACE/target/my-app-1.0-SNAPSHOT.jar'
-}
