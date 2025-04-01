7008b3b2c98dcff85a61808b09b3206c11f1bdda
Bharath Kumar N R <Bharath.Kumar.N.R@ibm.com>
2025-04-01 16:04:30 +0530

    Updated Jenkinsfile and calculator.groovy

--- vars/caculator.groovy
+++
@@ -1,9 +0,0 @@
-def add(x,y)
-{
-    println("sum of ${x} and ${y}: "+(x+y))
-}
-
-def sub(a,b)
-{
-    println("sub of ${a} and ${b}: "+(x-y))
-}
--- vars/sharedLib.groovy
+++
@@ -1,5 +0,0 @@
-def call()
-{
-    sh 'mvn clean package -DskipTests=true'
-    stash name: 'buildArtifacts', includes: '**/target/*.jar'
-}
